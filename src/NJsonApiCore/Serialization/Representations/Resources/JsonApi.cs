2f63a7bc312d509c2e18fbd28e1eb517be36df33
Rob <rob@myclinicaloutcomes.com>
2016-06-13 15:32:46 +0100

    Fixed deviation from the spec: JsonApi version was Uppercase.

--- src/NJsonApiCore/Serialization/Representations/Resources/JsonApi.cs
+++ src/NJsonApiCore/Serialization/Representations/Resources/JsonApi.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace NJsonApi.Serialization.Representations.Resources
 {
     public class JsonApi
     {
+        [JsonProperty(PropertyName = "version", NullValueHandling = NullValueHandling.Ignore)]
         public string Version => "1.0";
     }
 }
