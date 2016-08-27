2d27898caf74c0f2436b1d8cefe6d60d9db85671
Rob <rob@myclinicaloutcomes.com>
2016-08-27 16:20:41 +0100

    Pre-release 1.0.4

--- src/NJsonApiCore.Web.MVCCore/Properties/AssemblyInfo.cs
+++ src/NJsonApiCore.Web.MVCCore/Properties/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ed4f18ff-7d61-4413-b2ea-96dda140a442")]
-[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
