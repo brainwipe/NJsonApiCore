a9abc17c63d44e96b03552128dfede62e35b0672
Rob <rob@myclinicaloutcomes.com>
2016-09-05 16:46:52 +0100

    Updated version numbers for release 1.0.5.

--- src/NJsonApiCore.Web.MVC5/Properties/AssemblyInfo.cs
+++ src/NJsonApiCore.Web.MVC5/Properties/AssemblyInfo.cs
@@ -20,16 +20,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("806263c3-7903-4f1f-80c7-28793b51181b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
