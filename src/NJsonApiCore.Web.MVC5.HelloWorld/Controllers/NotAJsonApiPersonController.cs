7b3a6ea2e539e03f9efdd8e464e6326c40307284
Rob <rob@myclinicaloutcomes.com>
2017-02-16 11:14:26 +0000

    Changed the MVC5 action controller so that controllers that are not registered are ignored. For #33

---
+++ src/NJsonApiCore.Web.MVC5.HelloWorld/Controllers/NotAJsonApiPersonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using NJsonApi.Web.MVC5.HelloWorld.Models;
+
+namespace NJsonApiCore.Web.MVC5.HelloWorld.Controllers
+{
+    [RoutePrefix("notajsonapiperson")]
+    public class NotAJsonApiPersonController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return StaticPersistentStore.People;
+        }
+    }
+}
